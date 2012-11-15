ca00b5619fbaa2104e7c1acfabcee63040231098
Nuno Rosa <credential.helper>
2012-11-15 21:40:18 +0000

    removed github app secrets from source control

--- server/conf.js
+++ server/conf.js
@@ -1,6 +1,6 @@
 module.exports = {
      github: {
-        appId: '73df18986facb5bb049d',
-        appSecret: 'b3f44ed9926402ec4f3a86b87317ea05c084fccc'
+        appId: process.env.GITHUB_APP_ID,
+        appSecret: process.env.GITHUB_AP_SECRET
     }
 }
