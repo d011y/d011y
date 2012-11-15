d4076c3479056b20c225dbde25ae6e9584fdad9a
Nuno Rosa <credential.helper>
2012-11-15 03:18:29 +0000

    server github tests

---
+++ server/conf.js
@@ -0,0 +1,6 @@
+module.exports = {
+     github: {
+        appId: '73df18986facb5bb049d',
+        appSecret: 'b3f44ed9926402ec4f3a86b87317ea05c084fccc'
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var conf = require('./conf');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+var app = express(),
+    server = require('http').createServer(app);
+
+var port = process.env.PORT || 80;
+var staticDirPath = __dirname + '/public';
+
+passport.serializeUser(function(user, done) {
+  console.log(user);
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  console.log(obj);
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+    clientID: conf.github.appId,
+    clientSecret: conf.github.appSecret,
+    callbackURL: "http://localhost/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
+
+app.configure(function() {
+  app.use(express.logger());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/auth/github',
+  passport.authenticate('github'),
+  function(req, res){
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  }
+ );
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+    console.log(req.user);
+  }
+);
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/login', function(req, res){
+  req.send("not authenticated");
+});
+
+app.get('/', function(req, res){
+  res.send(req.user);
+});
+
+
+server.listen(port);
+
+console.log("Serving static files from " + staticDirPath + " @ port " + port);
