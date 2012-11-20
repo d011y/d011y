de835f092df807e0337bbd5ddfeafc67395b258c
Nuno Rosa <credential.helper>
2012-11-20 23:25:50 +0000

    angular app boostrap

---
+++ client/d011y/app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+d011yApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
---
+++ client/d011y/app/scripts/services/userservice.js
@@ -0,0 +1,15 @@
+'use strict';
+
+d011yApp.factory('userservice', function() {
+  // Service logic
+  // ...
+
+  var meaningOfLife = 42;
+
+  // Public API here
+  return {
+    someMethod: function() {
+      return meaningOfLife;
+    }
+  };
+});
